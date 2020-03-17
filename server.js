d99284755d01435d8eed1115564eb217fdb1b354
Iakov Salikov <isalikov@pm.me>
2020-03-17 18:08:57 +0200

    feat: update deply envs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const config = require('./config');
 const Socket = require('socket.io');
 const service = require('./services/apps.service');
 const Worker = require('./services/worker');
+const inspect = require('./libs/inspect');
 
 const server = http.createServer();
 const io = Socket(server);
@@ -10,3 +11,5 @@ const worker = new Worker(io);
 
 server.listen(config.get('PORT'));
 io.on('connection', service(worker));
+
+inspect(process.env);
